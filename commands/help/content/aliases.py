83caf557c001b33761ef96130a26734e3e42ed84
nikkiwritescode <nicole.farris@outlook.com>
2023-02-24 04:38:19 -0600

    Added very preliminary support for converting between Itadaki Street Wii and Fortune Street (#3)

--- commands/help/content/aliases.py
+++ commands/help/content/aliases.py
@@ -6,7 +6,9 @@ address_converter_aliases = [
     f"`{prefix}bsv2fsv` => `bsvtofsv` \n",
     f"`{prefix}bsv2isv` => `bsvtoisv` \n",
     f"`{prefix}fsv2bsv` => `fsvtobsv` \n",
+    f"`{prefix}fsv2isv` => `fsvtoisv` \n",
     f"`{prefix}isv2bsv` => `isvtobsv` \n",
+    f"`{prefix}isv2fsv` => `isvtofsv` \n",
     f"`{prefix}bsv2bsf` => `bsvtobsf` \n",
     f"`{prefix}fsv2fsf` => `fsvtofsf` \n",
 ]
